ad4bf01804bec252a4c06ab141750b595db3d7c8
Chase Devitt <55821619+CJdev99@users.noreply.github.com>
2022-09-03 01:02:38 -0500

    Update test_controller_node.py

--- snowplow_tesctrl_pkg/snowplow_tesctrl_pkg/test_controller_node.py
+++ snowplow_tesctrl_pkg/snowplow_tesctrl_pkg/test_controller_node.py
@@ -21,18 +21,34 @@ Rmotor_DIR = board.get_pin('d:8:o') # define RMotor direction-> IN2
 # Eventually will have node for each motor, when PID controller is implemented
 def Motor_Signals(v = 0, yaw_rate = 0):
     # linear v, angV ranges from -.7 to +.7, angular -0.4 to +0.4 respectively
+    Lmotor_calc = abs(v)-yaw_rate/2
+    if Lmotor_calc <= 0:
+            Lmotor_calc = 0
+        elif Lmotor_calc >= 200: # check if 200 is max
+            Lmotor_calc = 200
+            
+     Rmotor_calc = abs(v)+yaw_rate/2
+        if Rmotor_calc <= 0:
+            Rmotor_calc = 0
+        elif Rmotor_calc >= 200: # check if 200 is max
+            Rmotor_calc = 200
+            
+            
     if v < 0:
         Lmotor_DIR.write(0) # if v is neg, set digital write low
-        Lmotor_speed.write(-v*2)
-
+       
+        Lmotor_speed.write(Lmotor_calc) # joystick to the right, yaw rate will be (-), and we want the right motor to slow
+        # Joystick to the left, yaw_rate will
+        # Will have to tune yaw_rate multiplier thru testing
+        
         Rmotor_DIR.write(0)
-        Rmotor_speed.write(-v)
+        Rmotor_speed.write(Rmotor_calc) 
     else:
         Lmotor_DIR.write(1) # if v is neg, set direction backwards
-        Lmotor_speed.write(v*2)
+        Lmotor_speed.write(Lmotor_calc)
 
         Rmotor_DIR.write(1) # if v is neg, set direction backwards
-        Rmotor_speed.write(v)
+        Rmotor_speed.write(Rmotor_calc)
 
 
 
