6d984158acfb3934fc18a043d090940a3ba30312
Chase Devitt <55821619+CJdev99@users.noreply.github.com>
2022-12-07 21:30:47 -0600

    Update gps.py

--- jetson_snowplow/gps_tcp_publisher/scripts/gps.py
+++ jetson_snowplow/gps_tcp_publisher/scripts/gps.py
@@ -17,3 +17,37 @@ if __name__ == "__main__":
             gps.nmea_stream()
         except KeyboardInterrupt:
             gps.preempt()
+            
+    """
+    test:
+    #!/usr/bin/env python
+
+import rospy
+import serial
+import time
+import socket
+from std_msgs.msg import Header, Float64
+from sensor_msgs.msg import NavSatFix, NavSatStatus
+from TrimbleBD990 import TrimbleBD990
+
+def main():
+    # Initialize node
+    rospy.init_node("trimble_bd990_node")
+
+    # Create TrimbleBD990 object
+    bd990 = TrimbleBD990("192.168.1.100", 5017, 1024, 9600, 1)
+
+    # Open connection to device
+    bd990.open()
+
+    # Start NMEA stream
+    bd990.nmea_stream()
+
+    # Close connection to device
+    bd990.close()
+
+
+if __name__ == "__main__":
+    main()
+  """
+    
